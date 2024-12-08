1760acdace50c481a9dacc54eea79ff97edd9484
Davide Mendolia <davide@gokarumi.com>
2024-12-08 21:40:33 +0100

    Create Day06.kt

---
+++ src/Day06.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val map = input.map {
+            val zipWithNext = it.split(": ").zipWithNext { a, b ->
+                Pair(a.toLong(), b.split(" ").map(String::toLong))
+            }.first()
+            zipWithNext
+        }
+
+    }
+
+    val testInput = readInput("Day06_test")
+    val part1 = part1(testInput)
+    part1.println()
+    check(part1 == 41)
+}
